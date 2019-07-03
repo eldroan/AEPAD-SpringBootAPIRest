43aaf57e2baff6b041623bf4d8690c73f0085c97
eldroan <leandroamarillo95@gmail.com>
2019-07-03 00:41:38 -0300

    Se modificaron PedidoServiceDefault y OrdenCompraServiceDefault para que se generen con datos aleatorios y sea mas facil hacer las pruebas de jmeter

--- src/main/java/isi/aepad/service/impl/OrdenCompraServiceDefault.java
+++ src/main/java/isi/aepad/service/impl/OrdenCompraServiceDefault.java
@@ -1,19 +1,33 @@
 package isi.aepad.service.impl;
 
+import isi.aepad.domain.Cliente;
+import isi.aepad.domain.DetalleOrdenCompra;
 import isi.aepad.domain.OrdenCompra;
+import isi.aepad.domain.Producto;
+import isi.aepad.repository.ClienteRepository;
 import isi.aepad.repository.OrdenCompraRepository;
+import isi.aepad.repository.ProductoRepository;
 import isi.aepad.service.OrdenCompraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
+@SuppressWarnings("Duplicates")
 @Service
 public class OrdenCompraServiceDefault implements OrdenCompraService {
 
     @Autowired
     private OrdenCompraRepository repository;
 
+    //Estos metodos me los traigo para que sea mas facil crear un pedido
+    @Autowired
+    private ProductoRepository productorepository;
+    @Autowired
+    private ClienteRepository clienterepository;
     @Override
     public OrdenCompra buscarPorId(Integer id) {
         return repository.findById(id).orElseThrow(() -> new RuntimeException("No se encontro el cliente con ID "+id));
@@ -21,6 +35,45 @@ public class OrdenCompraServiceDefault implements OrdenCompraService {
 
     @Override
     public OrdenCompra guardar(OrdenCompra t) {
+        Random r = new Random();
+        List<Cliente> clientes = clienterepository.findAll();
+        Cliente clienteRandom = clientes.get(r.nextInt(clientes.size()));
+
+        int cantidadDeProductos = r.nextInt(7) + 4; //cantidad aleatorea entre 4 y 10;
+
+        List<Producto> productos = productorepository.findAll();
+        List<Producto> productoRandom = new ArrayList<>();
+        List<Integer> numberList = new ArrayList<>();
+
+        int counter = 0;
+
+        while(counter < cantidadDeProductos){
+            int currentInt = r.nextInt(productos.size());
+
+            if(numberList.contains(currentInt) == false){
+                numberList.add(currentInt);
+                productoRandom.add(productos.get(currentInt));
+                counter++;
+            }
+        }
+        List<DetalleOrdenCompra> detalleOrdenComprasRandom = new ArrayList<>();
+
+        for(int i = 0; i< cantidadDeProductos; i++){
+            DetalleOrdenCompra doc = new DetalleOrdenCompra();
+            doc.setProducto(productoRandom.get(i));
+            doc.setCantidad(r.nextInt(10));
+            doc.setPrecio(productoRandom.get(i).getPrecio());
+            doc.setOrdenDeCompra(t);
+            doc.setTotal(doc.getPrecio()*doc.getCantidad());
+
+            detalleOrdenComprasRandom.add(doc);
+        }
+
+        t.setCliente(clienteRandom);
+        t.setFecha(new Date());
+        t.setNumeroDeCompra((double)r.nextInt(1000000));
+        t.setDetalleOrdenCompras(detalleOrdenComprasRandom);
+
         return repository.save(t);
     }
 
--- src/main/java/isi/aepad/service/impl/PedidoServiceDefault.java
+++ src/main/java/isi/aepad/service/impl/PedidoServiceDefault.java
@@ -1,18 +1,32 @@
 package isi.aepad.service.impl;
 
-import isi.aepad.domain.Pedido;
+import isi.aepad.domain.*;
+import isi.aepad.repository.ClienteRepository;
 import isi.aepad.repository.PedidoRepository;
+import isi.aepad.repository.ProductoRepository;
 import isi.aepad.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
+@SuppressWarnings("Duplicates")
 @Service
 public class PedidoServiceDefault implements PedidoService {
 
     @Autowired
     private PedidoRepository repository;
+
+    //Estos metodos me los traigo para que sea mas facil crear un pedido
+    @Autowired
+    private ProductoRepository productorepository;
+    @Autowired
+    private ClienteRepository clienterepository;
+
+
 
     @Override
     public Pedido buscarPorId(Integer id) {
@@ -21,7 +35,46 @@ public class PedidoServiceDefault implements PedidoService {
 
     @Override
     public Pedido guardar(Pedido t) {
-        return repository.save(t);
+
+        Random r = new Random();
+        List<Cliente> clientes = clienterepository.findAll();
+        Cliente clienteRandom = clientes.get(r.nextInt(clientes.size()));
+
+        int cantidadDeProductos = r.nextInt(7) + 4; //cantidad aleatorea entre 4 y 10;
+
+        List<Producto> productos = productorepository.findAll();
+        List<Producto> productoRandom = new ArrayList<>();
+        List<Integer> numberList = new ArrayList<>();
+
+        int counter = 0;
+
+        while(counter < cantidadDeProductos){
+            int currentInt = r.nextInt(productos.size());
+
+            if(numberList.contains(currentInt) == false){
+                numberList.add(currentInt);
+                productoRandom.add(productos.get(currentInt));
+                counter++;
+            }
+        }
+
+        List<DetallePedido> detallesPedidosRandom = new ArrayList<>();
+        for(int i = 0; i<cantidadDeProductos;i++){
+            DetallePedido dp = new DetallePedido();
+            dp.setPedido(t);
+            dp.setProducto(productoRandom.get(i));
+            dp.setCantidad(r.nextInt(10));
+            dp.setPrecio(productoRandom.get(i).getPrecio());
+            dp.setTotal(dp.getPrecio()*dp.getCantidad());
+
+            detallesPedidosRandom.add(dp);
+        }
+
+        t.setDetallePedidos(detallesPedidosRandom);
+        t.setCliente(clienteRandom);
+        t.setFecha(new Date());
+
+        return repository.save(t); //Aca estoy confiando que spring boot es piola y se va a dar cuenta que los detalles pedidos son nuevos
     }
 
     @Override
